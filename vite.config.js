70142612d9fc0bfe6aa52b521bd973125e293812
kithain <132007320+kithain@users.noreply.github.com>
2025-11-25 23:03:48 +0100

    Configure GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// remplace "ombre-de-la-spirale" par TON nom de repo exact
 export default defineConfig({
   plugins: [react()],
-  base: '/ombre-de-la-spirale/',
+  base: '/ombre-de-la-spirale/', 
 })
